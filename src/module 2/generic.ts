441122627cf8068c5298cea766ad1b9082176d8b
selim2066 <mdselimreza2066@gmail.com>
2025-11-12 12:17:13 +0600

    generic She

---
+++ src/module 2/generic.ts
@@ -0,0 +1,24 @@
+//dynamically generalize: Generic
+
+type GenericArray<T> = Array<T>;
+
+const friends: GenericArray<string> = ["shafi", "Hasib", "Ali", "Mehedi", "Rumon"]
+
+const futureWife : GenericArray<string> = ["BatNight","KillerMan", "SadaFul"]
+
+type She ={
+
+  name: string,
+  id: string,
+  relation: boolean
+  
+
+}
+
+const rabbatulBayit : GenericArray<She> =[{
+name: "KN",
+id:"262235",
+relation: false
+}]
+
+//console.log(rabbatulBayit)
