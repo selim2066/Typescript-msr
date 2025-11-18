bea3b8a52c49de3a507f58ca0bbd5b90c5c14f6f
selim2066 <mdselimreza2066@gmail.com>
2025-11-18 21:13:29 +0600

    typeGuard

--- src/module3 oop/typeGuard.ts
+++ src/module3 oop/typeGuard.ts
@@ -4,4 +4,19 @@
 //? in typeof
 
 
+type Alphaneumeric = number | string;
 
+const add = (num1:Alphaneumeric , num2: Alphaneumeric)=>{
+
+  if(typeof num1 ==="number" && typeof num2==="number"){
+    return num1 + num2;
+  } else{
+   return num1.toString() + num2.toString()
+  }
+}
+
+const add1=add(2,2)
+const add2=add("44", "22")
+const add3=add(4,"4")
+
+console.log(add1, add2, add3)
