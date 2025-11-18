dd4af34f6410d7b6cdc6a0242861aebf8d8285fe
selim2066 <mdselimreza2066@gmail.com>
2025-11-18 21:20:19 +0600

    in guard

--- src/module3 oop/typeGuard.ts
+++ src/module3 oop/typeGuard.ts
@@ -3,20 +3,39 @@
 
 //? in typeof
 
-
 type Alphaneumeric = number | string;
 
-const add = (num1:Alphaneumeric , num2: Alphaneumeric)=>{
-
-  if(typeof num1 ==="number" && typeof num2==="number"){
+const add = (num1: Alphaneumeric, num2: Alphaneumeric) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2;
-  } else{
-   return num1.toString() + num2.toString()
+  } else {
+    return num1.toString() + num2.toString();
   }
-}
+};
 
-const add1=add(2,2)
-const add2=add("44", "22")
-const add3=add(4,"4")
+const add1 = add(2, 2);
+const add2 = add("44", "22");
+const add3 = add(4, "4");
 
-console.log(add1, add2, add3)
+console.log(add1, add2, add3);
+
+//? in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`${user.name} and his rolwe is : ${user.role}`);
+  } else {
+    console.log(` ${user.name}`);
+  }
+};
+
+getUserInfo({ name: "Normal", role: "Admin" });
