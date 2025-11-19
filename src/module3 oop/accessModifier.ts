920cf812827defcd91ec4af33b86e0c0986bc141
selim2066 <mdselimreza2066@gmail.com>
2025-11-19 09:49:34 +0600

    accessModifier

---
+++ src/module3 oop/accessModifier.ts
@@ -0,0 +1,34 @@
+// access >> modify
+
+class BankAccount {
+  public readonly userId: number;
+  public userName: string;
+  private _userBalance: number;
+
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this._userBalance = userBalance;
+  }
+
+  // private addBalance(balance: number) {
+  //   this._userBalance = this._userBalance + balance;
+  // }
+
+    addBalance(balance: number) {
+    this._userBalance = this._userBalance + balance;
+  }
+}
+
+class StudentBankAccount extends BankAccount {
+  test() {
+   // this.userBalance; // cz its private
+  }
+}
+
+const mezbaBhaiAccount = new BankAccount(111, "Mezba", 20);
+
+mezbaBhaiAccount.addBalance(100);
+mezbaBhaiAccount.addBalance(50);
+
+console.log(mezbaBhaiAccount);
